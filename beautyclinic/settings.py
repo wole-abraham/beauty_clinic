d530a941789bf65edcfd37fa484be2dd036e0c13
wole Abraham <wole1702@gmail.com>
2025-01-26 18:32:34 -0800

    added host

--- beautyclinic/settings.py
+++ beautyclinic/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8h=82@8z=47e2#p)#eg5h=zrirb73+-*f_l^3-@zx3ce4^1k4i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.marynassifchbat.com', '127.0.0.1']
 
 
 # Application definition
